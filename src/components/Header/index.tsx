3683c9bc29814a4aa1acb2f5af2a62be1a4e1e5d
Camila <camilasatie.dev@gmail.com>
2021-04-17 17:54:33 -0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <img src="/images/logo.svg" alt="logo" className={styles.logo} />
+      </Link>
+    </header>
+  );
 }
